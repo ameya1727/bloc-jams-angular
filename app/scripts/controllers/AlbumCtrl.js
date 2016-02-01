b3aa1133a460272fa6b9c694bc904b83e06ad37c
ameya1727 <ameya1727@gmail.com>
2016-01-31 19:05:03 -0800

    Services2 Checkpoint Completed.

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,24 +1,18 @@
 (function() {
-<<<<<<< HEAD
-     function AlbumCtrl(Fixtures) {
+     function AlbumCtrl(Fixtures, SongPlayer) {
          this.albumData = [];
          for (var i=0; i < 12; i++) {
-             this.albumData = Fixtures.getAlbum();
+             this.songPlayer = SongPlayer;
              
-=======
+
      function AlbumCtrl() {
          this.albumData = [];
          for (var i=0; i < 12; i++) {
              this.albumData.push(angular.copy(albumPicasso));
->>>>>>> fd93bea2f7cab3663bb1e5db106c7e262b3f00a4
          }
      }
  
      angular
          .module('blocJams')
-<<<<<<< HEAD
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
-=======
-         .controller('AlbumCtrl', AlbumCtrl);
->>>>>>> fd93bea2f7cab3663bb1e5db106c7e262b3f00a4
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
